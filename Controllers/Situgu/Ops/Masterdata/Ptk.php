7f82dbe64f7a173a5bf82140b116a4c525be9279
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-09-27 20:26:46 +0700

    pembenahan url syncrone

--- Controllers/Situgu/Ops/Masterdata/Ptk.php
+++ Controllers/Situgu/Ops/Masterdata/Ptk.php
@@ -355,8 +355,8 @@ class Ptk extends BaseController
                 }
             }
 
-            var_dump(getenv('be.default.url'));
-            die;
+            // var_dump(getenv('be.default.url'));
+            // die;
 
             if ($checkAnySynToday == date('Y-m-d')) {
                 $resultBack = true;
@@ -370,8 +370,8 @@ class Ptk extends BaseController
                 $result = $apiLib->syncPtk($npsn, $tw);
 
                 if ($result) {
-                    var_dump($result);
-                    die;
+                    // var_dump($result);
+                    // die;
                     if ($result->status == 200) {
                         $response = new \stdClass;
                         $response->status = 200;
