01bf09c754c02153dc33a76fc666c09bed4e8565
dwihandoko-studio <dwi.handoko0511@gmail.com>
2025-01-23 11:21:25 +0700

    add locked spj

--- Controllers/Sigaji/Su/Masterdata/Refbulan.php
+++ Controllers/Sigaji/Su/Masterdata/Refbulan.php
@@ -378,7 +378,7 @@ class Refbulan extends BaseController
                     $this->_db->transRollback();
                     $response = new \stdClass;
                     $response->status = 400;
-                    $response->message = "Data gagal dikunci.";
+                    $response->message = "Data gagal dikunci. 1";
                     return json_encode($response);
                 }
             } else {
